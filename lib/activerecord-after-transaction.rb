346a2231c2246f04e7a33ebf122f847568d4082b
dtaniwaki <daisuketaniwaki@gmail.com>
2014-11-09 00:06:18 +0900

    Fix loading error

--- lib/activerecord-after-transaction.rb
+++ lib/activerecord-after-transaction.rb
@@ -1,6 +1,6 @@
 require 'active_record'
-require 'activerecord_after_transaction/version'
-require 'activerecord_after_transaction/methods'
+require 'activerecord-after-transaction/version'
+require 'activerecord-after-transaction/methods'
 
 ActiveSupport.on_load :active_record do
   include ActiveRecord::AfterTransaction::Methods
